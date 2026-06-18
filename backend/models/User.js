71c956106faf4f5ce5619ef9453bf05c01aa1ac2
cpdoojt-dotcom <cpdo.ojt@gmail.com>
2026-06-18 09:15:43 +0800

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -15,7 +15,7 @@ const userSchema = new mongoose.Schema({
   },
   role: {
     type: String,
-    enum: ['admin', 'staff', 'otmps'],
+    enum: ['admin', 'otmps'],
     default: 'admin',
   },
   createdAt: {
